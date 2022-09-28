770c27d2af209d35a83744b779da86f2c8788f5f
XjKrb <wmjmwc@gmail.com>
2022-09-28 10:48:46 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     let nfts; 
     console.log("fetching nfts");
     
-    const api_key = ""
+    const api_key = "jP5c00dHZ53S3aR9Vs1y9j9mSIJPLEia"
 
 
 
@@ -44,7 +44,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = ""
+      const api_key = "jP5c00dHZ53S3aR9Vs1y9j9mSIJPLEia"
 
 
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
